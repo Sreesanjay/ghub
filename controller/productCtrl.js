9490af04f0ce9bc03ee0dadad0a175041fa5697e
Sreesanjay <sreesanjay7592@gmail.com>
2023-10-08 11:43:40 +0530

    category filter completed

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -4,61 +4,55 @@ const mongoose = require("mongoose");
 const asyncHandler = require("express-async-handler");
 
 
-function similarity(s1, s2) {
-     let longer = s1;
-     let shorter = s2;
-     if (s1.length < s2.length) {
-          longer = s2;
-          shorter = s1;
-     }
-     let longerLength = longer.length;
-     if (longerLength === 0) {
-          return 1.0;
-     }
-     return (
-          (longerLength - editDistance(longer, shorter)) /
-          parseFloat(longerLength)
-     );
-}
+// function similarity(s1, s2) {
+//      let longer = s1;
+//      let shorter = s2;
+//      if (s1.length < s2.length) {
+//           longer = s2;
+//           shorter = s1;
+//      }
+//      let longerLength = longer.length;
+//      if (longerLength === 0) {
+//           return 1.0;
+//      }
+//      return (
+//           (longerLength - editDistance(longer, shorter)) /
+//           parseFloat(longerLength)
+//      );
+// }
 
-function editDistance(s1, s2) {
-     s1 = s1.toLowerCase();
-     s2 = s2.toLowerCase();
-     s1 = s1.replace(/\s/g, "")
-     s2 = s2.replace(/\s/g, "")
-
-
-
-     let costs = new Array();
-     for (let i = 0; i <= s1.length; i++) {
-          let lastValue = i;
-          for (let j = 0; j <= s2.length; j++) {
-               if (i == 0) costs[j] = j;
-               else {
-                    if (j > 0) {
-                         let newValue = costs[j - 1];
-                         if (s1.charAt(i - 1) != s2.charAt(j - 1))
-                              newValue =
-                                   Math.min(
-                                        Math.min(newValue, lastValue),
-                                        costs[j]
-                                   ) + 1;
-                         costs[j - 1] = lastValue;
-                         lastValue = newValue;
-                    }
-               }
-          }
-          if (i > 0) costs[s2.length] = lastValue;
-     }
-     return costs[s2.length];
-}
+// function editDistance(s1, s2) {
+//      s1 = s1.toLowerCase();
+//      s2 = s2.toLowerCase();
+//      s1 = s1.replace(/\s/g, "")
+//      s2 = s2.replace(/\s/g, "")
 
 
 
-const sheckSubstring=(str,sub)=>{
+//      let costs = new Array();
+//      for (let i = 0; i <= s1.length; i++) {
+//           let lastValue = i;
+//           for (let j = 0; j <= s2.length; j++) {
+//                if (i == 0) costs[j] = j;
+//                else {
+//                     if (j > 0) {
+//                          let newValue = costs[j - 1];
+//                          if (s1.charAt(i - 1) != s2.charAt(j - 1))
+//                               newValue =
+//                                    Math.min(
+//                                         Math.min(newValue, lastValue),
+//                                         costs[j]
+//                                    ) + 1;
+//                          costs[j - 1] = lastValue;
+//                          lastValue = newValue;
+//                     }
+//                }
+//           }
+//           if (i > 0) costs[s2.length] = lastValue;
+//      }
+//      return costs[s2.length];
+// }
 
-
-}
 
 const viewProduct = async (req, res, next) => {
      try {
@@ -99,6 +93,13 @@ const filterProducts = asyncHandler(async (req, res, next) => {
                }
           }
      ])
+     if(req.query.category){
+          const filterKey=req.query.category
+          products=products.filter((product)=>{
+               return filterKey.includes(product.category.cat_name)
+          })
+     }
+     if(req.query.search){
      products = products.filter((product) => {
           const searchTerm = req.query.search.toLowerCase().replace(/\s/g, "");
 
@@ -118,8 +119,11 @@ const filterProducts = asyncHandler(async (req, res, next) => {
           }
            
       });
+     }
 
-      res.render('user/filterList',{products,search:req.query.search,userData:res.locals.userData})
+      const category=await Category.find({is_delete:false,cat_status:true})
+
+      res.render('user/filterList',{products,search:req.query.search,userData:res.locals.userData,category})
 
 })
 
